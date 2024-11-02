d6c5befd4687060c78a6898edc8fac3f3473a0e2
Clinamenic <ssc@clinamenic.com>
2024-11-01 22:53:07 -0700

    tweaks to footer

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -60,27 +60,33 @@ export default ((opts?: Options) => {
 
           </div>
 
-          <div className="footer-section">
-            <div className="nav-button">
+          <div className="footer-section" style="padding-top: 0rem; padding-bottom: 0.75rem; justify-content: space-between;">
+            <div className="nav-button" style="margin-top: 0.75rem;">
+              <a className="nav-link" href="https://bsky.app/profile/clinamenic.bsky.social">Bluesky</a>
+            </div>
+            <div className="nav-button" style="margin-top: 0.75rem;">
               <a className="nav-link" href="https://warpcast.com/clinamenic">Farcaster</a>
             </div>
-            <div className="nav-button">
+            <div className="nav-button" style="margin-top: 0.75rem;">
               <a className="nav-link" href="https://www.github.com/clinamenic">Github</a>
             </div>
-            <div className="nav-button">
+            <div className="nav-button" style="margin-top: 0.75rem;">
+              <a className="nav-link" href="https://letterboxd.com/clinamenic/">Letterboxd</a>
+            </div>
+            <div className="nav-button" style="margin-top: 0.75rem;">
               <a className="nav-link" href="https://www.x.com/clinamenic">Twitter</a>
             </div>
           </div>
 
           <div className="footer-section" style="border: 0px; margin-top: 0rem; justify-content: left;">
-            <p style="padding: 0rem 0rem 0rem 0rem; margin: 0rem 1rem 0rem 1rem; font-size: 0.8rem; justify-content: left; text-align: left;">
+            <p style="padding: 0rem 0rem 0rem 0rem; margin: 0rem 0.75rem 0rem 0.75rem; font-size: 0.8rem; justify-content: left; text-align: left;">
               Created with&nbsp;
               <a href="https://quartz.jzhao.xyz/">Quartz v4.3.1</a>
               &nbsp;© 2024<br />
               Published to Arweave via&nbsp;
               <a href="https://protocol.land/#/repository/4323e52e-edf7-4f09-8d3c-b9add2bb14cb">Protocol.land</a><br />
-              Hosted on IPFS via Fleek<br />
-              Designed by Clinamenic LLC
+              Hosted on IPFS via <a href="https://bafybeid3ql53i7y42jgphmj52vv76c5joxgmkadpsdhvc764vc6rmhlu2q.ipfs.flk-ipfs.xyz/">Fleek</a><br />
+              Designed by <a href="https://www.clinamenic.com">Clinamenic LLC</a>
             </p>
           
           </div>
